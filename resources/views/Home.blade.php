fc9ea965843fc517954bc96b96a72940863f95ab
Md Ariful hossain Anuwar <mdanuwar96@gmail.com>
2023-01-01 08:39:19 +0600

    Home Table setup

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,37 @@
+@extends('layout.app');
+@section('content')
+    
+<div class="container">
+    <div class="d-flex justify-content-between">
+        <a href="#" class="btn btn-primary">Add Data</a>
+        <a href="#" class="btn btn-info">Dowload All data</a>
+    </div>
+    <table class="table">
+        <thead>
+          <tr>
+            <th>Student Id</th>
+            <th>Name</th>
+            <th>Home</th>
+            <th>Photo</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>20231</td>
+            <td>Akash</td>
+            <td>mymensingh</td>
+            <td>Photo</td>
+            <td>
+                <div class="gap-3 d-flex">
+                    <a href="#" class="btn btn-info">Edit</a>
+                    <a href="#" class="btn btn-danger">Delete</a>
+                </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+</div>
+
+
+@endsection
